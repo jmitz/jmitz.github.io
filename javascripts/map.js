cc0339a1c797906d067b9df23d9f2b0c9c3236e9
Jeff Mitzelfelt <jdmitz@gmail.com>
2015-12-29 17:30:31 -0600

    Updated style on leaflet popup, Added Open Street Map Attribution to map

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,7 +1,9 @@
 var map = new L.Map('divMap',
 	{center: new L.LatLng(40.5, -89.5), zoom: 8});
 
-var baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+var baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 function onLocationFound(e) {
    var radius = e.accuracy / 2;
    L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters of this point").openPopup();
