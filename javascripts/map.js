28c3863a802ca2e823a3b8fb00b818197d3dd070
Jeff Mitzelfelt <jdmitz@gmail.com>
2015-12-29 16:01:11 -0600

    Updated Leaflet version

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,9 +1,11 @@
 var map = new L.Map('divMap',
 	{center: new L.LatLng(40.5, -89.5), zoom: 8});
 var googleLayer = new L.Google('ROADMAP');
+
 map.addLayer(googleLayer);
 
 function onLocationFound(e) {
+	console.log(e);
    var radius = e.accuracy / 2;
    L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
    L.circle(e.latlng, radius).addTo(map);
