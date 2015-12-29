48a5d42cb5b20caa86b62a915ebf8b032d96a839
Jeff Mitzelfelt <jdmitz@gmail.com>
2015-12-29 17:20:44 -0600

    Forgot to add base layer to map.

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,16 +1,15 @@
 var map = new L.Map('divMap',
 	{center: new L.LatLng(40.5, -89.5), zoom: 8});
 
-var baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+var baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 function onLocationFound(e) {
-	console.log(e);
    var radius = e.accuracy / 2;
    L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters of this point").openPopup();
    L.circle(e.latlng, radius).addTo(map);
 }
 
 function onLocationError(e) {
-   alert(e.message);
+   console.log(e.message);
 }
 
 map.on('locationfound', onLocationFound);
