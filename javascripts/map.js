03c6a3f1ae0f03c2710be1bfe6bdabbaed88fff0
Jeff Mitzelfelt <jdmitz@gmail.com>
2015-12-29 17:16:31 -0600

    Added Open Street Map Https????

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,10 +1,11 @@
 var map = new L.Map('divMap',
 	{center: new L.LatLng(40.5, -89.5), zoom: 8});
 
+var baseMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 function onLocationFound(e) {
 	console.log(e);
    var radius = e.accuracy / 2;
-   L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+   L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters of this point").openPopup();
    L.circle(e.latlng, radius).addTo(map);
 }
 
